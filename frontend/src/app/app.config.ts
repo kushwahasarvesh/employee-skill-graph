e1a976e8ff18d13446d67803b7bb341daba90fb0
sarvesh.kushwaha <sarvesh.kushwaha@comviva.com>
2026-08-11 09:42:31 +0530

    update for deployment

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch  } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withFetch()),            
-    provideClientHydration(withEventReplay())
+    provideHttpClient(withFetch())
   ]
 };
